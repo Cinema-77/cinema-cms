a6eb92b838e900df319c6b5fab4198bdb39380e5
Long Ngo <ngobi0617@gmail.com>
2021-11-28 16:47:18 +0700

    CNM-16 Add filter group sort for revenue

--- src/features/revenue/components/TableRevenue.tsx
+++ src/features/revenue/components/TableRevenue.tsx
@@ -1,12 +1,82 @@
-import { Thead, Tbody, chakra, Table, Tr, Th, Td, Icon } from '@chakra-ui/react';
+import {
+  Thead,
+  Tbody,
+  chakra,
+  Table,
+  Tr,
+  Th,
+  Td,
+  Icon,
+  Input,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItemOption,
+  MenuOptionGroup,
+  Stack,
+} from '@chakra-ui/react';
 import * as React from 'react';
-// import { GoTriangleDown, GoTriangleUp } from 'react-icons/go';
-import { FiArrowRight, FiArrowDown, FiXCircle, FiBox } from 'react-icons/fi';
-import { useTable, useGroupBy, useExpanded } from 'react-table';
+import { FiArrowRight, FiArrowDown } from 'react-icons/fi';
+import { GoTriangleDown, GoTriangleUp } from 'react-icons/go';
+import {
+  useTable,
+  useGroupBy,
+  useExpanded,
+  useSortBy,
+  useAsyncDebounce,
+  useGlobalFilter,
+} from 'react-table';
+// import matchSorter from 'match-sorter';
 
 import { mapDataRevenue } from '@/features/revenue';
 import { formatNumber } from '@/utils/format';
 
+const MenuFilter = ({ headerGroups }: any) => {
+  return (
+    <Menu closeOnSelect={false}>
+      <MenuButton as={Button} colorScheme="blue">
+        Nhóm theo
+      </MenuButton>
+      <MenuList minWidth="240px">
+        <MenuOptionGroup title="Nhóm" type="checkbox">
+          {headerGroups[1].headers
+            .filter((column: any) => column.canGroupBy)
+            .map((column: any, index: number) => (
+              <MenuItemOption
+                value={column.render('Header')}
+                {...column.getGroupByToggleProps()}
+                key={index}
+              >
+                {column.render('Header')}
+              </MenuItemOption>
+            ))}
+        </MenuOptionGroup>
+      </MenuList>
+    </Menu>
+  );
+};
+const GlobalFilter = ({ preGlobalFilteredRows, globalFilter, setGlobalFilter }: any) => {
+  const count = preGlobalFilteredRows.length;
+  const [value, setValue] = React.useState(globalFilter);
+  const onChange = useAsyncDebounce((value) => {
+    setGlobalFilter(value || undefined);
+  }, 200);
+
+  return (
+    <chakra.span display="flex" alignItems="center">
+      <Input
+        variant="outline"
+        placeholder={`Search: ${count} records...`}
+        value={value || ''}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+      />
+    </chakra.span>
+  );
+};
 interface TableRevenueProps {
   rowsTable: any;
 }
@@ -19,7 +89,6 @@ export const TableRevenue: React.FC<TableRevenueProps> = ({ rowsTable }) => {
       {
         Header: 'Thông tin',
         Footer: 'Thông tin',
-
         columns: [
           {
             Header: 'Tên phim',
@@ -47,10 +116,12 @@ export const TableRevenue: React.FC<TableRevenueProps> = ({ rowsTable }) => {
           {
             Header: 'Số lượng',
             accessor: 'quantity',
+            canGroupBy: false,
           },
           {
             Header: 'Đơn giá',
             accessor: 'price',
+            canGroupBy: false,
           },
           {
             Header: 'Loại',
@@ -59,6 +130,7 @@ export const TableRevenue: React.FC<TableRevenueProps> = ({ rowsTable }) => {
           {
             Header: 'Tổng',
             accessor: 'totalString',
+            canGroupBy: false,
             Footer: (info: any) => {
               // Only calculate total visits if rows change
 
@@ -78,103 +150,105 @@ export const TableRevenue: React.FC<TableRevenueProps> = ({ rowsTable }) => {
     [],
   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, footerGroups, rows, prepareRow } =
-    useTable({ columns, data }, useGroupBy, useExpanded);
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+    state,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+  } = useTable({ columns, data }, useGroupBy, useGlobalFilter, useSortBy, useExpanded) as any;
 
   return (
-    <Table {...getTableProps()}>
-      <Thead>
-        {headerGroups.map((headerGroup) => (
-          <Tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
-            {headerGroup.headers.map((column: any) => (
-              <Th {...column.getHeaderProps()} isNumeric={column.isNumeric} key={column.id}>
-                {column.canGroupBy ? (
-                  // If the column can be grouped, let's add a toggle
-                  <chakra.span {...column.getGroupByToggleProps()} mr={2}>
-                    {column.isGrouped ? (
-                      <Icon as={FiXCircle} aria-label="Close group" />
-                    ) : (
-                      <Icon as={FiBox} aria-label=" group" />
-                    )}
-                  </chakra.span>
-                ) : null}
-                {column.render('Header')}
-                {/* <chakra.span pl="4">
-                  {column.isSorted ? (
-                    column.isSortedDesc ? (
-                      <Icon as={GoTriangleDown} aria-label="sorted descending" />
-                    ) : (
-                      <Icon as={GoTriangleUp} aria-label="sorted ascending" />
-                    )
-                  ) : null}
-                </chakra.span> */}
-              </Th>
-            ))}
-          </Tr>
-        ))}
-      </Thead>
-      <Tbody {...getTableBodyProps()}>
-        {rows.map((row: any) => {
-          prepareRow(row);
-          return (
-            <Tr {...row.getRowProps()} key={row.id}>
-              {row.cells.map((cell: any, index: number) => (
-                <Td
-                  // For educational purposes, let's color the
-                  // cell depending on what type it is given
-                  // from the useGroupBy hook
-                  key={index}
-                  {...cell.getCellProps()}
-                  //   style={{
-                  //     background: cell.isGrouped
-                  //       ? '#0aff0082'
-                  //       : cell.isAggregated
-                  //       ? '#ffa50078'
-                  //       : cell.isPlaceholder
-                  //       ? '#ff000042'
-                  //       : 'white',
-                  //   }}
+    <>
+      <Stack spacing={2} direction="row" justifyContent="flex-end">
+        <GlobalFilter
+          preGlobalFilteredRows={preGlobalFilteredRows}
+          globalFilter={state.globalFilter}
+          setGlobalFilter={setGlobalFilter}
+        />
+        <MenuFilter headerGroups={headerGroups} />
+      </Stack>
+
+      <Table {...getTableProps()}>
+        <Thead>
+          {headerGroups.map((headerGroup: any) => (
+            <Tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
+              {headerGroup.headers.map((column: any) => (
+                <Th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  isNumeric={column.isNumeric}
+                  key={column.id}
                 >
-                  {cell.isGrouped ? (
-                    // If it's a grouped cell, add an expander and row count
-                    <>
-                      <chakra.span {...row.getToggleRowExpandedProps()}>
-                        {row.isExpanded ? (
-                          <Icon as={FiArrowDown} aria-label="expanded" />
-                        ) : (
-                          <Icon as={FiArrowRight} aria-label="unexpanded" />
-                        )}
-                      </chakra.span>{' '}
-                      {cell.render('Cell')} ({row.subRows.length})
-                    </>
-                  ) : cell.isAggregated ? (
-                    // If the cell is aggregated, use the Aggregated
-                    // renderer for cell
-                    cell.render('Aggregated')
-                  ) : cell.isPlaceholder ? null : ( // For cells with repeated values, render null
-                    // Otherwise, just render the regular cell
-                    cell.render('Cell')
-                  )}
-                </Td>
-                // <Td key={index} {...cell.getCellProps()} isNumeric={cell.column.isNumeric}>
-                //   {cell.render('Cell')}
-                // </Td>
+                  {column.render('Header')}
+                  <chakra.span pl="4">
+                    {column.isSorted ? (
+                      column.isSortedDesc ? (
+                        <Icon as={GoTriangleDown} aria-label="sorted descending" />
+                      ) : (
+                        <Icon as={GoTriangleUp} aria-label="sorted ascending" />
+                      )
+                    ) : null}
+                  </chakra.span>
+                </Th>
               ))}
             </Tr>
-          );
-        })}
-      </Tbody>
-      <tfoot>
-        {footerGroups.map((group: any) => (
-          <tr {...group.getFooterGroupProps()} key={group.id}>
-            {group.headers.map((column: any) => (
-              <td {...column.getFooterProps()} key={column.id}>
-                {column.render('Footer')}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tfoot>
-    </Table>
+          ))}
+        </Thead>
+        <Tbody {...getTableBodyProps()}>
+          {rows.map((row: any) => {
+            prepareRow(row);
+            return (
+              <Tr {...row.getRowProps()} key={row.id}>
+                {row.cells.map((cell: any, index: number) => (
+                  <Td
+                    // For educational purposes, let's color the
+                    // cell depending on what type it is given
+                    // from the useGroupBy hook
+                    key={index}
+                    {...cell.getCellProps()}
+                  >
+                    {cell.isGrouped ? (
+                      // If it's a grouped cell, add an expander and row count
+                      <>
+                        <chakra.span {...row.getToggleRowExpandedProps()}>
+                          {row.isExpanded ? (
+                            <Icon as={FiArrowDown} aria-label="expanded" />
+                          ) : (
+                            <Icon as={FiArrowRight} aria-label="unexpanded" />
+                          )}
+                        </chakra.span>{' '}
+                        {cell.render('Cell')} ({row.subRows.length})
+                      </>
+                    ) : cell.isAggregated ? (
+                      // If the cell is aggregated, use the Aggregated
+                      // renderer for cell
+                      cell.render('Aggregated')
+                    ) : cell.isPlaceholder ? null : ( // For cells with repeated values, render null
+                      // Otherwise, just render the regular cell
+                      cell.render('Cell')
+                    )}
+                  </Td>
+                ))}
+              </Tr>
+            );
+          })}
+        </Tbody>
+        <tfoot>
+          {footerGroups.map((group: any) => (
+            <tr {...group.getFooterGroupProps()} key={group.id}>
+              {group.headers.map((column: any) => (
+                <td {...column.getFooterProps()} key={column.id}>
+                  {column.render('Footer')}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </Table>
+    </>
   );
 };
